abcc250a6370302ea23321d0a4a12b4841a3e03c
naresh <nareshnalla456@gmail.com>
2020-04-11 11:25:43 +0530

    sucess

--- Condition.java
+++ Condition.java
@@ -2,8 +2,9 @@ class Condition{
 public static void main(String[]ar){
 
 int n=100;
-for(int i=1;i<=100;i++){
-System.out.print(" "+(i*i*i));
+for(int i=1;i<=n;i++){
+	
+System.out.print(" "+(i*i+"="i));
 
 }
 }
